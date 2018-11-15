dca74944ee74300aea303969c5e0218d3d3a9d17
Home <alexey.khomiak@gmail.com>
2018-11-15 21:03:46 +0200

    first

---
+++ XamarinClient/XamarinClient/Models/TodoList.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinClient
+{
+    public class TodoList
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public string Notes { get; set; }
+        public bool Done { get; set; }
+
+    }
+}
---
+++ XamarinClient/XamarinClient/View/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinClient
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://192.168.0.100:52950/api/ToDoListApi/";
+        private readonly HttpClient _client = new HttpClient();
+        public ObservableCollection<TodoList> _tasks;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            string content = await _client.GetStringAsync(Url);
+            List<TodoList> tasks = JsonConvert.DeserializeObject<List<TodoList>>(content);
+            _tasks = new ObservableCollection<TodoList>(tasks);
+            listViewTodoList.ItemsSource = _tasks;
+            base.OnAppearing();
+        }
+
+        private async void OnAdd(object sender, EventArgs e)
+        {
+            TodoList post = new TodoList { TaskName = Title.Text, Notes = Description.Text, Done =false };
+            
+            string content = JsonConvert.SerializeObject(post); 
+            await _client.PostAsync(Url, new StringContent(content, Encoding.UTF8, "application/json"));
+            _tasks.Insert(0, post);
+        }
+        
+        private async void OnDelete(object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            int index = _tasks.IndexOf((TodoList)e.Item);
+            await _client.DeleteAsync(Url + "/" + index);
+            _tasks.RemoveAt(index);
+        }
+    }
+}
